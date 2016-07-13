04ddc2977aace1d882de50c002de05611f046046
Minamiya <yukihiratype2@live.com>
2016-07-13 17:35:27 +0800

    update

--- js/signIn.js
+++ js/signIn.js
@@ -29,5 +29,36 @@ function login() {
     $('.alert-danger').css("display","block");
     return 1;
   }
+  //============================================================================
+
+
+  //Use Ajax to request a Token
+  var xmlhttpToken;
+  if (window.XMLHttpRequest) {
+    xmlhttpToken = new XMLHttpRequest();
+  } else {
+    xmlhttpToken = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttpToken.open("GET","/chat/php/verfilelogin.php?requireToken=True",false);
+  xmlhttpToken.send();
+  var token = xmlhttpToken.responseText;
+  //============================================================================
+
+
+  //Mix token and password to transport,later
+  //TO DO
+  var email = $('#inputEmail').val();
+  var passwd = md5($('input[type="password"]').val());
+  var sendContent = "Email=" + email + "&Passwd=" + passwd + "&receiveToken=" + token;
+  alert(sendContent);
+  var xmlhttpVerfi;
+  if (window.XMLHttpRequest) {
+    xmlhttpVerfi = new XMLHttpRequest;
+  } else {
+    xmlhttpVerfi = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttpVerfi.open("POST","/chat//php/verfilelogin.php",false);
+  xmlhttpVerfi.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  xmlhttpVerfi.send(sendContent);
 
 }
--- php/verfilelogin.php
+++ php/verfilelogin.php
@@ -1,3 +1,21 @@
 <?php
 
+  //Initializing variables
+         $token = "";
+         $Email = "";
+        $Passwd = "";
+  $receiveToken = "";
+  //Check if declared
+       $token = isset($_GET['requireToken']) ? $_GET['requireToken'] : '';
+       $Email = isset($_POST['email'])       ?       $_POST['email'] : '';
+      $Passwd = isset($_POST['passwd'])      ?      $_POST['passwd'] : '';
+$receiveToken = isset($_POST['token'])       ?       $_POST['token'] : '';
+
+
+
+  if ($token != "") {
+    echo ($localToken = substr(time(),-5));
+  } else {
+    # code...
+  }
  ?>
