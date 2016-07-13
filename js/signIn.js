43c68e2a41b102bf48c3ffdeea5029816eed2a92
Minamiya <yukihiratype2@live.com>
2016-07-13 11:30:16 +0800

    Update

--- js/signIn.js
+++ js/signIn.js
@@ -1,3 +1,28 @@
 $(function() {
-  
+  $('button[type=submit]').click(function(){
+    event.preventDefault();
+    login();
+  });
+  $('.form-signin').submit(function(){
+    event.preventDefault();
+    login();
+  });
 });
+
+function login() {
+  var emailValue = $('#inputEmail').val();
+  var PasswdValue = $('input[type=password]').val();
+
+  //verify input
+  if (emailValue == "") {
+    $('#errorMessage').append("Please input Email");
+    $('.alert-danger').css("display","block");
+    return 1;
+  }
+  if (PasswdValue == "") {
+    $('#errorMessage').append("Please input Password");
+    $('.alert-danger').css("display","block");
+    return 1;
+  }
+
+}
