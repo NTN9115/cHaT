5500ca5e666463730229b1af28b557331e9fcc3a
Minamiya <yukihiratype2@live.com>
2016-07-13 21:44:45 +0800

    update

--- php/databaseSignUp.php
+++ php/databaseSignUp.php
@@ -1,16 +1,23 @@
 <?php
 
-  $server = "127.0.0.1";
+  $server = "192.168.24.156:3306";
   $user = "root";
-  $passwd = "";
+  $passwd = "876412";
 
   //Connect to DB
   $connect = mysqli_connect($server,$user,$passwd);
   if (!$connect) {
-    die('Connect failed')
+    die('Connect failed');
   }
-  echo "good";
+  mysqli_autocommit($connect,TRUE);
+  mysql_select_db($connect,basic_user);
   //============================================================================
+
+  //Insert User
+  $userData = "INSERT INTO basic_user (user_email,user_name,user_passwd) Vaules (";
+  $userData = $userData.Email.userName.password_hash($Passwd,PASSWORD_DEFAULT).")";
+  mysqli_query($connect,$userData);
+
 
   //close connect
   mysqli_close($conn);
