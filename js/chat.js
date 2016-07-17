5656779a2371148958f132bfc2e46a8588bf1256
Minamiya <yukihiratype2@live.com>
2016-07-17 12:31:27 +0800

    Update

---
+++ js/chat.js
@@ -0,0 +1,23 @@
+$(function(){
+
+});
+
+function sendChat() {
+  //Use Ajax to send chat
+}
+
+function receiveChat() {
+  //Use setTimeIntervel to pull chat from server
+}
+
+function reveiveChatList() {
+  //every 2s pull chat list form server
+}
+
+function addFriend() {
+  //Add friend
+}
+
+function viewChanger() {
+  //Set form suitable
+}
