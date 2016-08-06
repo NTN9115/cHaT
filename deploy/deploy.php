de60ddfa6bf3d036094d86ad23a0d56c8b08245c
Yukihiratype2 <yukihiratype2@live.com>
2016-08-06 10:41:01 +0800

    Fix deploy

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -15,8 +15,23 @@
     $output .= trim($tmp)."\n";
   }
 
-  //output result
-  if ($POST['payload']) {
-    echo $output;
-  }
  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Deploy</title>
+</head>
+<body>
+  <p>
+    <?php
+    //output result
+    if ($POST['payload']) {
+      echo $output;
+    }
+     ?>
+  </p>
+</body>
+</html>
