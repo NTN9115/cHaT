060d63e572c5c036523ce39b2be7ab62fecb94c2
Yukihiratype2 <yukihiratype2@live.com>
2016-08-06 21:10:15 +0800

    test

--- index.php
+++ index.php
@@ -2,8 +2,5 @@
   $title = "Welcome cHaT";
   $lessSheetFile = "index";
 
-  $content = "<h1 class=\"text-center\">Welcome</h1>";
   include 'templet/genelTemplet.php';
-  include '/content/index.html';
-
 ?>
