40dd3509cb8490b1579d3a1acbaac91ecdd17ea6
Yukihiratype2 <yukihiratype2@live.com>
2016-08-06 13:02:23 +0800

    Add bootstrap

--- templet/genelTemplet.php
+++ templet/genelTemplet.php
@@ -4,11 +4,16 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+
   <title><?php echo $title ?></title>
 </head>
 <body>
   <?php
     echo $indexContent;
    ?>
+   <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
+   <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
 </html>
