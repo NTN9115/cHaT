1ef6c5daaac55203db03637e65be85163bbfce90
Yukihiratype2 <yukihiratype2@live.com>
2016-08-06 19:29:22 +0800

    test

--- templet/genelTemplet.php
+++ templet/genelTemplet.php
@@ -49,7 +49,7 @@
 
 <!-- Include content file -->
   <?php
-    require "/content/index.html";
+    require "/content/index.php";
    ?>
    <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
    <script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
