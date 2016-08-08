e9a1162976b3ea2d4d679e42e17f2ee4c1ca1e3b
Yukihiratype2 <yukihiratype2@live.com>
2016-08-08 21:54:50 +0800

    update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   if (PATH_SEPARATOR == ":") {
     set_include_path('/var/www/html/');
   }else {
-    set_include_path('c:/wamp/www/cHat/');
+    // set_include_path('c:/wamp/www/cHat/');
   }
   $title = "Welcome cHaT";
   $lessSheetFile = "index";
--- userAction/login.php
+++ userAction/login.php
@@ -1,5 +1,9 @@
 <?php
+if (PATH_SEPARATOR == ":") {
   set_include_path('/var/www/html/');
+}else {
+  set_include_path('c:/wamp/www/cHat/');
+}
   $title = "Login";
   $lessSheetFile = "login";
 
