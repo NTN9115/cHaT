ea8fa1fff59004e251cc0ba30ea8ac47b16a9651
Yukihiratype2 <yukihiratype2@live.com>
2016-08-11 19:00:26 +0800

    damn I'm good

--- js/action.js
+++ js/action.js
@@ -1,10 +1,26 @@
-$(document).ready(function() {
-  if ($.url().param('target') == "signin") {
-    $('collapseSignUpForm').collapse('hide');
-  }else {
-    $('collapseSignInForm').collapse('hide');
+$.extend({
+  urlparam: function(target) {
+    var reg = new RegExp("(^|&)" + target + "=([^&]*)(&|$)");
+    var result = window.location.search.substr(1).match(reg);
+    if (result != null) {
+      return unescape(result[2]);
+    }
+    return null;
   }
-  $('button[type=submit]').click(function(event) {
+});
+
+$(document).ready(function() {
+  if ($.urlparam('target') != "signin") {
+    $('.actionForm').collapse('toggle');
+    $('.btn-block').toggleClass('folded');
+  }
+  $('.btn-block').click(function(event) {
     event.preventDefault();
+    if ($(this).hasClass('folded')) {
+    $('.actionForm').collapse('toggle');
+    $('.btn-block').toggleClass('folded');
+  }else {
+
+  }
   });
 });
