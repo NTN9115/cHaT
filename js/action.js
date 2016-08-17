3ebc0a0c76ca36c1229d9f170f62d20dde1b9000
Yukihiratype2 <yukihiratype2@live.com>
2016-08-17 16:40:10 +0800

    update

--- js/action.js
+++ js/action.js
@@ -20,7 +20,31 @@ $(document).ready(function() {
       $('.actionForm').collapse('toggle');
       $('.btn-block').toggleClass('folded');
     }else {
-      
+      var token = "";
+
+      $.ajax({
+        url: '/userAction/auth.php',
+        type: 'GET',
+        dataType: 'html',
+        data: {"flag" :"requestToken"},
+        async: false,
+      })
+      .done(function(data) {
+        console.log("success");
+        console.log(token = data);
+        $('input[name=token]').val(token);
+      })
+      .fail(function() {
+        console.log("error");
+      })
+      .always(function() {
+        console.log("complete");
+      });
+
+      console.log($(this).parent().serialize());
+      $.post('/userAction/auth.php', $(this).parent().serialize(), function(data, textStatus, xhr) {
+        console.log(data);
+      });
     }
   });
 });
--- userAction/auth.php
+++ userAction/auth.php
@@ -3,14 +3,21 @@
   //1. bowser use ajax with in requestToken flag to request a token;
   //2. server send a token to bowser;
   //3. server accept signin flag,userEmail,hased password and the token had been sent use POST;
+  //  3.1 if no flag received,return 403
   //4. server query database;
-  //5. server callback use ajax;
+  //  4.1 if userEmail already using,return 400
+  //  4.2 if userEmail & userName do not match,return 402
+
   $flag = $_POST['flag'];
+  if ($flag == "") {
+    $flag = $_GET['flag'];
+  }
   $userName = $_POST['userName'];
   $userEmail = $_POST['userEmail'];
   $passWd = $_POST['passwd'];
   $token = $_POST['token'];
   set_include_path('/var/www/html/');
+  set_include_path('/Library/WebServer/Documents');
 
   $passWd = password_hash($passWd,PASSWORD_DEFAULT);
 
@@ -18,6 +25,7 @@
     session_start();
     $_SESSION['token'] = time();
     echo password_hash($_SESSION['token'],PASSWORD_DEFAULT);
+    exit();
   }else {
     session_start();
     if (password_verify($_SESSION['token'],$token)) {
@@ -29,18 +37,18 @@
 
         //check if user exist
         if (mysqli_num_rows(mysqli_query($conn,"select * from userBasic where userEmail = \"{$userEmail}\"")) != 0) {
-          echo 400;
+          exit("400");
         }else {
-          //TODO
+          mysqli_query($conn,"insert into userBasic (userName,userEmail,userPW) values (\"{$userName}\",\"{$userEmail}\",\"{$passWd}\")");
         }
       }else {
         die("Error Request");
       }
       $_SESSION['Online'] = true;
-      echo 200;
+      exit("200");
     }else {
       $_SESSION['Online'] = false;
-      echo 403;
+      exit("403");
     }
   }
  ?>
